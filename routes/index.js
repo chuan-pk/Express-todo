e9cad5619d07ad6243a69eefba5be75c61175d16
Khuna Chuan <s5901012610091@email.kmutnb.ac.th>
2018-04-12 19:45:31 +0700

    can get todo-text POST request

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Todo dashboard' });
 });
 
+router.post('/', function(req, res){
+    var text = req.body.todo_text;              // get todo text from input name = 'todo_text'
+    var html_text = "<li>" + text + "</li>"     
+    res.render('index', {title: 'Todo dashboard', todo_text: html_text});
+})
+
 module.exports = router;
