f2a0cffce91b4b966b8765810489bc3fdf053261
Khuna Chuan <s5901012610091@email.kmutnb.ac.th>
2018-04-24 08:42:36 +0700

    edit .twig file for show todo and complete item

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,11 @@ var models = require('../db/models');
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    models.Todo.findAll().then(function(todo) {
+    models.Todo.findAll({
+        // where:{
+        //     complete: false             // find all todo in database, todo-list's complete attribute = false 
+        // }
+    }).then(function(todo){
       res.render('index', { title: 'Todo dashboard', todo_list: todo});  
         });
 });
@@ -23,5 +27,19 @@ router.post('/', function(req, res){
     });
     res.redirect('/')
 });
+
+
+router.post('/submit', function(req, res){
+    var item_id = req.body.item_id;
+    var item = models.Todo.findById(item_id);
+    item.complete = not(item.complete);
+    item.save();
+});
+
+router.post('/delete', function(req, res){
+    var item_id = req.body.item_id;
+    // delete item   
+})
+
 
 module.exports = router;
