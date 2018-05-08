8b362107c08bc025bab40f8db24f1e46a926cdb7
Khuna Chuan <s5901012610091@email.kmutnb.ac.th>
2018-05-08 11:57:14 +0700

    merge signin and signup page into one page

--- routes/signup.js
+++ routes/signup.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.get('/', function(req, res, next) {
-  res.render('signup', {title: 'Sign Up'});
-});
+router.get('/',function(req,res){
+  res.redirect('/');
+  });
 
 router.post('/', passport.authenticate('local-signup', {
     successRedirect: '/', 
